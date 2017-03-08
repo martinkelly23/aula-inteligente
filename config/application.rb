d6e23d0c809b8052b1b283b566d8e16b26b3842f
martinkelly23 <martkelly92@gmail.com>
2017-03-08 17:38:53 -0300

    Se agregaron los gliphycon

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-
+config.assets.paths << Rails.root.join("vendor","assets", "fonts")
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,7 +21,7 @@ module Laboratorio7
     config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
