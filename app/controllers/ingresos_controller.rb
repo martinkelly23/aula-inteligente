aed4b30f1202c654f1754f18cb283a29cde04cbe
martinkelly23 <martkelly92@gmail.com>
2017-04-10 18:50:26 -0300

    Correccion hora entrada y salida

--- app/controllers/ingresos_controller.rb
+++ app/controllers/ingresos_controller.rb
@@ -10,7 +10,7 @@ class IngresosController < ApplicationController
    if(@ingreso.HoraEntrada == 1) # Significa que es una entrada
      @ingreso.horaEnt = Time.zone.now
      @ingreso.ConsumoParcial = 0
-     @aula = Aula.find(@ingreso.aula_id) #Busco el aula de la cual el usuario sale
+     @aula = Aula.find(@ingreso.aula_id) #Busco el aula de la cual el usuario ingresa
      @aula.estado = "En uso"
      @aula.save #Guardo la modificacion hecha en el aula
      if(cumple)
