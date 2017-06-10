ca580e9004ee3e0ddf34e2d6a34738fee079901d
martinkelly23 <martkelly92@gmail.com>
2017-06-10 18:14:15 -0300

    prueba sin fecha en vista

--- app/controllers/ingresos_controller.rb
+++ app/controllers/ingresos_controller.rb
@@ -8,12 +8,12 @@ class IngresosController < ApplicationController
     cumple = true
 
    if(@ingreso.HoraEntrada == 1) # Significa que es una entrada
+     #@ingreso.horaEnt = Time.zone.now
      @ingreso.ConsumoParcial = 0
      @aula = Aula.find(@ingreso.aula_id) #Busco el aula de la cual el usuario ingresa
      @aula.estado = "En uso"
      @aula.save #Guardo la modificacion hecha en el aula
      if(cumple)
-        @ingreso.horaEnt = Time.zone.now
        if @ingreso.save
          flash[:notice] = 'El nuevo ingreso se registro exitosamente!'
          redirect_to listadoIngresos_path
@@ -28,6 +28,7 @@ class IngresosController < ApplicationController
    if(@ingreso.HoraSalida == 1) # Significa que es una salida
      @ingresoAux = Ingreso.where(aula_id: @ingreso.aula_id).first
      @ingresoAux.horaSal = Time.zone.now
+     @ingresoAux.horaEnt = Time.zone.now
      @ingresoAux.HoraSalida = 1
      @ingresoAux.ConsumoParcial = @ingreso.ConsumoParcial
      @aula = Aula.find(@ingreso.aula_id) #Busco el aula de la cual el usuario sale
